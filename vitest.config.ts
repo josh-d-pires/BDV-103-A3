dd27ad4122f9f81673a09db5b6d33e8498cfa08f
Josh Pires <josh@pires.ca>
2026-02-14 16:30:47 +0000

    54816+

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     includeSource: ['src/**/*.{js,ts}'],
-    // setupFiles: ['./database_test_setup.ts']
+    setupFiles: ['./database_test_setup.mts']
   }
 })
